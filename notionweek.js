8f8d2f67998277bbefa3c698202368f86e307881
adihd <47402232+adihd@users.noreply.github.com>
2020-07-14 20:00:42 +0300

    comit

--- notionweek.js
+++ notionweek.js
@@ -3,17 +3,22 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.4
 // @description  Aasdasdasdadsasdasdad
-// @author       adi strauss
+// @author       adi
 // @include      https://www.notion.so/*
 // @grant        none
 // ==/UserScript==
 
 (function () {
 
+
+
+    // Select the node that will be observed for mutations
+
+
     // check if there is a calander view names "week_cal"
     // if so go to the calander block
     function minifunc() {
-        console.log("this is working1 mini");
+        // console.log("this is working1 mini");
         var days_list = document.getElementsByClassName("adi_week_cal")[0].getElementsByClassName("notion-calendar-view-day")
         for (let index = 0; index < days_list.length; index++) {
             // const element = array[index];
@@ -40,19 +45,55 @@
     }
 
     var foo = function () {
-        console.log("this is working1");
+        // console.log("this is working1");
         var view_block_list = document.getElementsByClassName("notion-collection-view-select")
         for (let index = 0; index < view_block_list.length; index++) {
-            console.log("this is working2");
+            // console.log("this is working2");
             if (view_block_list[index].innerText === "week_cal") {
-                console.log("this is working3");
+                // console.log("this is working3");
                 view_block_list[index].parentElement.parentElement.parentElement.parentElement.classList.add("adi_week_cal")
                 minifunc()
             }
         }
     };
 
-    setTimeout(foo, 5000);
+
+
+
+
+
+    // Select the node that will be observed for mutations
+    const targetNode = document;
+
+    // Options for the observer (which mutations to observe)
+    const config = {
+        attributes: true,
+        childList: true,
+        subtree: true
+    };
+
+    // Callback function to execute when mutations are observed
+    const callback = function (mutationsList, observer) {
+        // Use traditional 'for loops' for IE 11
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                // console.log('A child node has been added or removed.');
+                setTimeout(foo, 100);
+
+            } else if (mutation.type === 'attributes') {
+                // console.log('The ' + mutation.attributeName + ' attribute was modified.');
+            }
+        }
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+
+    // Later, you can stop observing
+    // observer.disconnect();
 
 
 
